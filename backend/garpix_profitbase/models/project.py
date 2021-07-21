0a59c83fb4920ddd47f5f81371f2cfcc5ab92450
oem <azarov@garpix.com>
2021-07-22 00:58:37 +0300

    добавил релейтед нэйм к ФК

--- backend/garpix_profitbase/models/project.py
+++ backend/garpix_profitbase/models/project.py
@@ -9,7 +9,7 @@ ProjectMixin = import_string(settings.GARPIX_PROFITBASE_PROJECT_MIXIN)
 class Project(ProjectMixin, models.Model):
     name = models.CharField(max_length=100, verbose_name='Название')
     profitbase_id = models.IntegerField(verbose_name='ProfitBase ID')
-    city = models.ForeignKey(City, on_delete=models.CASCADE, verbose_name='Город')
+    city = models.ForeignKey(City, on_delete=models.CASCADE, verbose_name='Город', related_name='project')
 
     def __str__(self):
         return self.title
